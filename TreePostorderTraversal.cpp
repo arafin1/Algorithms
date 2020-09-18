813b704bda1cfbf7fcab247f90f864ee2fe79593
Arafin1 <arafinsaadmahmdud@gmail.com>
2020-09-18 21:02:53 +0600

    Hackerrank Tree: Postorder Traversal

---
+++ TreePostorderTraversal.cpp
@@ -0,0 +1,8 @@
+ void postOrder(Node *root) {
+        if(root == NULL){
+            return;
+        }
+        postOrder(root->left);
+        postOrder(root->right);
+        std::cout<<root->data<<" ";
+    }
