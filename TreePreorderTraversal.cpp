fab99da16048f9529fe4260eb5b3c8b4283054d9
Arafin1 <arafinsaadmahmdud@gmail.com>
2020-09-18 20:59:47 +0600

    Hackerrank Tree: Inorder Traversal

---
+++ TreePreorderTraversal.cpp
@@ -0,0 +1,8 @@
+void preOrder(Node *root) {
+        if(root == NULL){
+            return;
+        }
+        std::cout<<root->data<<" ";
+        preOrder(root->left);
+        preOrder(root->right);
+    }
