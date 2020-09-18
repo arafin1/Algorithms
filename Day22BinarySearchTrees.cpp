a6f63d0cf4f45635a0ad5239fd73c1287d391834
Arafin1 <arafinsaadmahmdud@gmail.com>
2020-09-18 21:03:37 +0600

    Hackerrank Day 22: Binary Search Trees
    Height

---
+++ Day22BinarySearchTrees.cpp
@@ -0,0 +1,8 @@
+int getHeight(Node* root){
+         if(root == NULL){
+            return -1;
+        }
+
+        return (max(getHeight(root->left),
+                   getHeight(root->right)) + 1) ;
+        }
