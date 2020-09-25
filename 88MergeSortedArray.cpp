e5d6b8a723481a1d73fc391c24939994f08d7ca6
Arafin1 <arafinsaadmahmdud@gmail.com>
2020-09-25 23:49:40 +0600

    LeetCode 88. Merge Sorted Array

---
+++ 88MergeSortedArray.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+       /* Runtime : 0ms
+          memory: 9.4mb
+        if(n == 0){
+            return;
+        }
+       int ns = 0;
+        n=n-1;
+       for(int i = m; i<nums1.size();i++){
+           nums1[i] = nums2[n];
+           n--;
+           
+       }
+        sort(nums1.begin(),nums1.end());
+
+       */
+        //Runtime : 4ms
+        //Memory: 9mb
+         if(n == 0){
+            return;
+        }
+       int ns = 0;
+       for(int i = m; i<nums1.size();i++){
+           nums1[i] = nums2[ns];
+           ns++;
+           
+       }
+        sort(nums1.begin(),nums1.end());
+
+    }
+};
